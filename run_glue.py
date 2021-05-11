0d3814de6f61713eda711f1b0813408d6ec2f33c
Zeyu Liu <zeyuliu2@cs.washington.edu>
2021-05-10 19:10:15 -0700

    support pruning on mbart

--- run_glue.py
+++ run_glue.py
@@ -30,6 +30,7 @@ import transformers
 from transformers import (
     AutoConfig,
     AutoModelForSequenceClassification,
+    MBartForSequenceClassification,
     AutoTokenizer,
     EvalPrediction,
     HfArgumentParser,
@@ -287,7 +288,7 @@ def main():
         cache_dir=model_args.cache_dir,
         use_fast=model_args.use_fast_tokenizer,
     )
-    model = AutoModelForSequenceClassification.from_pretrained(
+    model = MBartForSequenceClassification.from_pretrained(
         model_args.model_name_or_path,
         from_tf=bool(".ckpt" in model_args.model_name_or_path),
         config=config,
